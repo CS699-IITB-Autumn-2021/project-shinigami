24050d760cdee1d1e3a3d00a0c2b327f8dbb11b3
Jaimin020 <jaiminchauhan23@gmail.com>
2021-10-24 12:57:07 +0530

    Encryption Key sen to user

--- docuFile/login/views.py
+++ docuFile/login/views.py
@@ -7,6 +7,11 @@ from django.contrib import auth
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
+from fpdf import FPDF
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import random
+from django.conf import settings
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
@@ -14,6 +19,38 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request, 'index.html')
 
+def gen_pdf(uname,key):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size = 15)
+    pdf.cell(200, 10, txt = "Hi,thank you for registering in docuFile.", ln = 1, align = 'L')
+    pdf.cell(200, 10, txt = "Your private key is "+str(key),ln = 2, align = 'L')
+    pdf.cell(200, 10, txt = "Please note that this private key is not stored in our servers.",ln = 3, align = 'L')
+    pdf.cell(200, 10, txt = "Hence you are supposed to keep this safe with you at all times.",ln = 4, align = 'L')
+    pdf.cell(200, 10, txt = "The Institute/Company you give access to your docs will also need this private key to ",ln = 5, align = 'L')
+    pdf.cell(200, 10, txt = "view your documents.",ln = 6, align = 'L')
+    pdf.cell(200, 10, txt = "So you will also have to share this with them.",ln = 6, align = 'L')
+    pdf.output(str(uname)+".pdf")
+
+def enc_pdf(uname,passwd):
+    out = PdfFileWriter()
+    file = PdfFileReader(uname+".pdf")
+    num = file.numPages
+    for idx in range(num):
+        page = file.getPage(idx)
+        out.addPage(page)
+    out.encrypt(passwd)
+    with open(uname+".pdf", "wb") as f:
+        out.write(f)
+
+def gen_mail(uname,email):
+    subject = 'welcome to docuFile'
+    message = f'Hi , thank you for registering in docuFile.\nYour private key is in the attach file.\nPassword of that file in your acc. password.'
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = ["jaiminchauhan23@gmail.com", ]
+    email = EmailMessage(subject, message, email_from, recipient_list)
+    email.attach_file(uname+'.pdf')
+    email.send()
 
 def register(request):
     if request.method == 'POST':
@@ -21,13 +58,18 @@ def register(request):
         if form.is_valid():
             user_type = request.POST.get('user_type', '')
             user_name = request.POST.get('username', '')
-            print(user_type)
+            passwd = request.POST.get('password1')
+            email = request.POST.get('email')
+            print(passwd)
             form.save()
             t = User.objects.get(username=user_name)
             u = UserType.objects.get(user_id=t.id)
             u.user_type = user_type
             u.user_name = user_name
             u.save()
+            gen_pdf(user_name,key = random.randint(1, 100))
+            enc_pdf(user_name,passwd)
+            gen_mail(user_name,email)
             return render(request, 'index.html')
         else:
             return render(request, 'register.html', {"msg": form.errors})
